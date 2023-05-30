4eb334cd167ca9f2e05d3d1dd75c66fa7dc164d5
Ale Lepere <alejandrolepere@gmail.com>
2023-05-29 23:14:52 -0300

    Primer cambio

---
+++ enviar_formulario.php
@@ -0,0 +1,28 @@
+
+
+if(isset($_POST['enviar'])){
+  
+    // Información del formulario
+  $nombre = $_POST['nombre'];
+  $correo = $_POST['correo'];
+  $mensaje = $_POST['mensaje'];
+  
+  // Destinatario del correo electrónico
+  $destinatario = "alejandrolepere@gmail.com";
+  
+  // Asunto del correo electrónico
+  $asunto = "Nuevo mensaje del formulario de contacto";
+  
+  // Cuerpo del correo electrónico
+  $cuerpo = "Nombre: " . $nombre . "\n";
+  $cuerpo .= "Correo electrónico: " . $correo . "\n";
+  $cuerpo .= "telefono: " . $telefono . "\n";
+  $cuerpo .= "Mensaje: " . $mensaje . "\n";
+  
+  // Enviar correo electrónico
+  if(mail($destinatario, $asunto, $cuerpo)){
+    echo "Gracias por su mensaje. Nos pondremos en contacto con usted pronto.";
+  } else{
+    echo "Ha ocurrido un error. Por favor, inténtelo de nuevo más tarde.";
+  }
+}
